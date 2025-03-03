f566bdf0e313009b6112a1b1f8ec200203a50b93
astrokompas <129834159+astrokompas@users.noreply.github.com>
2025-03-03 17:40:40 +0100

    added all necessary pages

---
+++ Pages/ClientsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using InvoicingApp.ViewModels;
+
+namespace InvoicingApp.Pages
+{
+    public partial class ClientsPage : Page
+    {
+        public ClientsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pages/InvoiceEditorPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using InvoicingApp.ViewModels;
+
+namespace InvoicingApp.Pages
+{
+    public partial class InvoiceEditorPage : Page
+    {
+        public InvoiceEditorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
